296c4785dc8fe89670ce9151661674d987dcf0ff
Hyosang Yoon <hyosang@mit.edu>
2015-04-13 22:12:19 -0400

    HelloWorld!

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.*;
+
+public class HelloWorld
+{
+	public static void main (String [] args) throws IOException
+	{    	
+		System.out.printf("Hello World!\n");
+	}
+}
